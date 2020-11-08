adc9ebd1c795b5765f300f23c747407791af5e62
mobil3d <72637100+mobil3d@users.noreply.github.com>
2020-11-08 15:23:37 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,23 +5,22 @@ var speed,weight;
 function setup() {
   createCanvas(1600,400);
 
+  speed = random(55,90);
+
   car = createSprite(50, 200, 50, 50);
   car.velocityX = speed;
 
   wall = createSprite(1500,200,50,height/2);
-  wall.shapeColor = (80,80,80) 
-
-car.velocityX = speed;
-
-  speed = random(55,90);
+  wall.shapeColor = color(80,80,80) 
+  
   weight = random(400,1500);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0,0,0);  
 
-  if(wall.x -car.x < (car.width + wall;;.width/2)
-  {
+  if(wall.x -car.x < (car.width + wall;;.width/2){
+  
   car.velocityX = 0;
   var deformation = 0.5 *weight*speed*speed/22509; 
   if(deformation>180)
@@ -39,21 +38,5 @@ if(deformation<100)
   car.shapeColor = color(0,255,0);
 }
  }
-
+ drawSprites();
   }
-
-
-
-
-
-
-
-
-
-  
-
-  drawSprites();
-}
-
-
-
