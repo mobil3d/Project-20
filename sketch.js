12ba704a313f7c811831b5979cfc9bb290b01955
mobil3d <72637100+mobil3d@users.noreply.github.com>
2020-11-14 15:40:50 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,9 +17,9 @@ function setup() {
 }
 
 function draw() {
-  background(0,0,0);  
+  background(0,0,0); 
 
-  if(wall.x -car.x < (car.width + wall;;.width/2){
+  if(wall.x -car.x < (car.width + wall.width/2)){
   
   car.velocityX = 0;
   var deformation = 0.5 *weight*speed*speed/22509; 
